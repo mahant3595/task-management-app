b6d5e9761d89b470ed70d199c13c0420d00f5bc9
Mahant T <mahantvibisha@gmail.com>
2026-05-25 10:42:51 +0530

    Update TaskForm component

---
+++ frontend/src/components/TaskForm.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+function TaskForm({ addTask }) {
+  const [task, setTask] = useState({
+    title: '',
+    description: ''
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(task);
+
+    setTask({
+      title: '',
+      description: ''
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Task Title"
+        value={task.title}
+        onChange={(e) => setTask({ ...task, title: e.target.value })}
+      />
+
+      <input
+        type="text"
+        placeholder="Description"
+        value={task.description}
+        onChange={(e) => setTask({ ...task, description: e.target.value })}
+      />
+
+      <button type="submit">Add Task</button>
+    </form>
+  );
+}
+
+export default TaskForm;

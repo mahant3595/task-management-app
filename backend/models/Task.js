cbaec864aab7b51fc4d612db19524403fc1049c5
Mahant T <mahantvibisha@gmail.com>
2026-05-25 10:44:24 +0530

    Update Task model

---
+++ backend/models/Task.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  userId: String
+});
+
+module.exports = mongoose.model('Task', taskSchema);
